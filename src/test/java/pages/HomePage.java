757ffc1e40ff52caaae3e0a1c0ac13e8971277aa
merve99celebi <merve99celebi@gmail.com>
2025-10-21 00:37:48 +0300

    Project created

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,4 +1,22 @@
 package pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ReusableMethods;
+
+public class HomePage extends ReusableMethods {
+
+    public HomePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//div/h1/span")
+    public WebElement homePageText;
+
+    @FindBy(xpath = "//a[@class='zak-button']")
+    public WebElement demoBtn;
+
+    @FindBy(xpath = "(//span[@class='elementor-button-text'])[4]")
+    public WebElement openMrs2Btn;
 }
