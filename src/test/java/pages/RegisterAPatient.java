5e91e8134c50836f50ef25ceadcd7850aec026ce
affan <affanergul@outlook.com>
2025-10-22 23:46:29 +0300

    TC_404 updated

---
+++ src/test/java/pages/RegisterAPatient.java
@@ -0,0 +1,62 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ReusableMethods;
+
+public class RegisterAPatient extends ReusableMethods {
+    public RegisterAPatient() {
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @FindBy(xpath = "//button[@aria-label='Add patient']")
+    public WebElement addPatientBtn;
+
+    @FindBy(xpath = "//input[@id='givenName']")
+    public WebElement firstName;
+
+    @FindBy(xpath = "//input[@id='familyName']")
+    public WebElement familyName;
+
+    @FindBy(css = "div[class='cds--radio-button-wrapper']")
+    public WebElement gender;
+
+    @FindBy(css = "div[id='birthdate']")
+    public WebElement birth;
+
+    @FindBy(xpath = "//input[@id='address1']")
+    public WebElement adress1;
+
+    @FindBy(xpath = "//input[@id='address2']")
+    public WebElement adress2;
+
+    @FindBy(xpath = "//input[@id='cityVillage']")
+    public WebElement city;
+
+    @FindBy(xpath = "//input[@id='stateProvince']")
+    public WebElement state;
+
+    @FindBy(xpath = "//input[@id='country']")
+    public WebElement country;
+
+    @FindBy(xpath = "//input[@id='postalCode']")
+    public WebElement postalCode;
+
+    @FindBy(xpath = "//input[@id='phone']")
+    public WebElement phoneNumber;
+
+    @FindBy(xpath = "//button[@class='-esm-patient-registration__patient-registration__submitButton___Ps1do cds--btn cds--btn--primary']")
+    public WebElement registerPatientBtn;
+
+
+
+
+
+
+}
+
+
+
+
