d09e8c0ed142f4887c666cad4e4717c25dc02ffa
ssibelcebeci <ssibelcebeci@gmail.com>
2025-10-24 01:43:07 +0400

    US_409_TC,AddPatientToQueue done.

--- src/test/java/US_409_TC.java
+++ src/test/java/US_409_TC.java
@@ -1,7 +1,5 @@
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import pages.DemoPage;
 import pages.HomePage;
@@ -9,38 +7,48 @@ import pages.LoginPage;
 import pages.AddPatientToQueue;
 import utilities.ReusableMethods;
 
+import java.util.Random;
+
 public class US_409_TC extends ReusableMethods {
 
-    HomePage homePage;
-    LoginPage loginPage;
-    AddPatientToQueue addPatientToQueue;
-    DemoPage demoPage;
-    String username="admin";
-    String password="Admin123";
-    String patientName="sam";
-
+    String username = "admin";
+    String password = "Admin123";
+    String patientName = "sam";
+    String selectValue = "On time";
 
     @Test
     public void addPatientToQueue() {
-        homePage = new HomePage();
-        loginPage = new LoginPage();
-        addPatientToQueue = new AddPatientToQueue();
-        demoPage = new DemoPage();
+        HomePage hp = new HomePage();
+        LoginPage lp = new LoginPage();
+        AddPatientToQueue aptq = new AddPatientToQueue();
+        DemoPage dp = new DemoPage();
+        Random random = new Random();
 
         driver.get("https://openmrs.org/");
-        myClick(homePage.demoBtn);
-        jsClick(demoPage.openMrs3Btn);
-        mySendKeys(loginPage.userNameInput, username);
-        myClick(loginPage.continueLoginBtn);
-        mySendKeys(loginPage.passwordInput, password);
-        myClick(loginPage.continueLoginBtn);
+        myClick(hp.demoBtn);
+        jsClick(dp.openMrs3Btn);
+        mySendKeys(lp.userNameInput, username);
+        myClick(lp.continueLoginBtn);
+        mySendKeys(lp.passwordInput, password);
+        myClick(lp.continueLoginBtn);
 
-        myClick(addPatientToQueue.addPatientToQueueBtn);
+        myClick(aptq.addPatientToQueueBtn);
 
-        myClick(addPatientToQueue.searchInput);
+        mySendKeys(aptq.searchInput, patientName);
 
-        mySendKeys(addPatientToQueue.searchInput, patientName);
+        myClick(aptq.startVisitBtn);
 
-        myClick(addPatientToQueue.startVisitBtn);
+        wait.until(ExpectedConditions.visibilityOfAllElements(aptq.visitType));
+        int randomIndex = random.nextInt(aptq.visitType.size());
+        myClick(aptq.visitType.get(randomIndex));
+
+        scrollToElement(aptq.punctuality);
+        wait.until(ExpectedConditions.visibilityOf(aptq.punctuality));
+        Select select = new Select(aptq.punctuality);
+        select.selectByVisibleText(selectValue);
+
+        scrollToElement(aptq.startVisitConfirmBtn);
+        wait.until(ExpectedConditions.visibilityOf(aptq.startVisitConfirmBtn));
+        myClick(aptq.startVisitConfirmBtn);
     }
 }
