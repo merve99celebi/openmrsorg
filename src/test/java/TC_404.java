620e821d7663a29e76316e7226c74f9cb9a3267d
affan <affanergul@outlook.com>
2025-10-24 18:59:26 +0300

    TC_404 Finish

--- src/test/java/TC_404.java
+++ src/test/java/TC_404.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DemoPage;
 import pages.HomePage;
@@ -6,6 +9,8 @@ import pages.LoginPage;
 import pages.RegisterAPatient;
 import utilities.BaseDriver;
 import utilities.ReusableMethods;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 public class TC_404 extends BaseDriver {
@@ -30,20 +35,28 @@ public class TC_404 extends BaseDriver {
         hp.myClick(dp.openMrs3Btn);
         wait.until(ExpectedConditions.elementToBeClickable(lp.verifyLoginPage));
         lp.mySendKeys(lp.userNameInput, "admin");
-        //lp.myClick(lp.);
+        lp.myClick(lp.continueLoginBtn);
         lp.mySendKeys(lp.passwordInput, "Admin123");
         ReusableMethods.threadWait(2);
-        //lp.myClick(lp.);
+        lp.myClick(lp.continueLoginBtn);
+        ReusableMethods.threadWait(2);
         rgp.myClick(rgp.addPatientBtn);
         rgp.mySendKeys(rgp.firstName, "marc");
         rgp.mySendKeys(rgp.familyName, "marces");
         rgp.myClick(rgp.gender);
-        rgp.myClick(rgp.birth);
+        ReusableMethods.threadWait(2);
+        rgp.myClick(rgp.dateofBirthNoBtn);
+        rgp.mySendKeys(rgp.estimatedAgeBtn, "4");
+        rgp.mySendKeys(rgp.adress1,"home1");
+        rgp.mySendKeys(rgp.adress2,"home2");
+        rgp.mySendKeys(rgp.city,"africa");
+        rgp.mySendKeys(rgp.state,"province");
+        rgp.mySendKeys(rgp.country,"gine");
+        rgp.mySendKeys(rgp.postalCode,"00322");
+        rgp.mySendKeys(rgp.phoneNumber,"234723946");
         rgp.myClick(rgp.registerPatientBtn);
 
-
-
-
+        Assert.assertTrue(driver.findElement(By.xpath("//button[@aria-label='Add patient']")).isDisplayed());
 
     }
 }
--- src/test/java/pages/RegisterAPatient.java
+++ src/test/java/pages/RegisterAPatient.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.ReusableMethods;
 
+import java.util.List;
+
 public class RegisterAPatient extends ReusableMethods {
     public RegisterAPatient() {
         PageFactory.initElements(driver, this);
@@ -49,6 +51,15 @@ public class RegisterAPatient extends ReusableMethods {
 
     @FindBy(xpath = "//button[@class='-esm-patient-registration__patient-registration__submitButton___Ps1do cds--btn cds--btn--primary']")
     public WebElement registerPatientBtn;
+
+    @FindBy(xpath = "(//span[@class='cds--content-switcher__label'])[4]")
+    public WebElement dateofBirthNoBtn;
+
+    @FindBy(xpath = "//input[@id='yearsEstimated']")
+    public WebElement estimatedAgeBtn;
+
+
+
 
 
 
